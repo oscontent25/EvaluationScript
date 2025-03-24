eb1a99020c1185990cb083d0837bbfbce709c76b
wingrew <1934247510@qq.com>
2025-03-24 20:08:40 +0800

    30

--- glibc/basic-glibc.test.js
+++ glibc/basic-glibc.test.js
@@ -33,23 +33,23 @@ let points = {
     'glibc test_write': [0, 2],
     'glibc test_yield': [0, 4]
 };
-// const { assert } = require('console');
-// const fs = require('fs');  // 引入 fs 模块
+const { assert } = require('console');
+const fs = require('fs');  // 引入 fs 模块
 
-// // 读取命令行传入的文件路径（在命令行中传入文件路径）
-// const filePath = process.argv[2];  // 通过 process.argv 获取传入的文件路径
+// 读取命令行传入的文件路径（在命令行中传入文件路径）
+const filePath = process.argv[2];  // 通过 process.argv 获取传入的文件路径
 
-// // 读取文件内容
-// fs.readFile(filePath, 'utf8', (err, data) => {
-//     if (err) {
-//         console.error("读取文件时发生错误:", err);
-//         return;
-//     }
+// 读取文件内容
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error("读取文件时发生错误:", err);
+        return;
+    }
 
-//     // 将文件内容传递给 judge 函数
-//     let result = judge(data);
-//     console.log("Judge function result:", result);  // 输出 judge 函数的返回值
-// });
+    // 将文件内容传递给 judge 函数
+    let result = judge(data);
+    console.log("Judge function result:", result);  // 输出 judge 函数的返回值
+});
 // Base assertion utility function
 function assertUtil(testFn, data, args, expectedPass, testName, pointsKey) {
     const result = testFn(data, ...args);
@@ -348,7 +348,9 @@ function judge(outputFile) {
         if (end === -1) {
             continue;
         }
-        const testOutput = outputFile.substring(start + startMatch[0].length, end)
+        let testOutputstr = outputFile.substring(start + startMatch[0].length, end).replace(/\r/g, '');
+        console.log(testOutputstr);
+        const testOutput = testOutputstr
         .split('\n')
         .filter(line => line)
         .map(line => line.startsWith(',') ? line.substring(1) : line);
