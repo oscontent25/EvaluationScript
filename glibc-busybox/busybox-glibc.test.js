b01cf30ab353319130115c86726e6e63659a5d53
wingrew <1934247510@qq.com>
2025-04-07 07:03:16 +0800

    fix

--- glibc-busybox/busybox-glibc.test.js
+++ glibc-busybox/busybox-glibc.test.js
@@ -35,7 +35,7 @@ let points = {
     'glibc busybox hexdump -C test.txt': [0, 1],
     'glibc busybox md5sum test.txt': [0, 1],
     'glibc busybox echo "ccccccc" >> test.txt': [0, 1],
-    'glibc busybox echo "bbbbbbb" >> test.txt': [0, 1],
+    'glibc busybox echo "bbbbbbb" >> test.txt': [0, 2],
     'glibc busybox echo "aaaaaaa" >> test.txt': [0, 1],
     'glibc busybox echo "2222222" >> test.txt': [0, 1],
     'glibc busybox echo "1111111" >> test.txt': [0, 1],
@@ -143,7 +143,7 @@ function judge(outputFile){
         if (Object.hasOwnProperty.call(points, key)) {
             let [first, second] = points[key];
             // 检查第一个数是否是第二个数的四倍
-            if (first === second * 4) {
+            if (first >= second * 4) {
                 points[key][0] = 1;  
                 points[key][1] = 1;  
             }else{
